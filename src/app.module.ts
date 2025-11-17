125dc300838a3789822deb9f94a7834c40f0350f
Bruno Pressi <brunopressi2012@gmail.com>
2025-11-17 15:06:40 -0300

    feat: add health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ClienteModule } from './cliente/cliente.module';
 import { EnderecoModule } from './endereco/endereco.module';
 import { CepModule } from './cep/cep.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [PrismaModule, ClienteModule, EnderecoModule, CepModule],
+  imports: [PrismaModule, ClienteModule, EnderecoModule, CepModule, HealthModule],
   controllers: [],
   providers: [],
 })
