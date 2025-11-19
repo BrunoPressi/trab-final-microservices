6d8898c0554db21d158f26d1c2de385f42a8a619
Bruno Pressi <brunopressi2012@gmail.com>
2025-11-19 15:23:23 -0300

    refactor: cepService

--- src/cep/cep.service.ts
+++ src/cep/cep.service.ts
@@ -4,16 +4,43 @@ import { InvalidCepException } from 'src/exception/InvalidCepException';
 
 @Injectable()
 export class CepService {
+  private axiosInstance = axios.create({
+    timeout: 5000,
+    family: 4,
+  });
+
   async validarCep(cep: string) {
-    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    try {
+      const url = `https://viacep.com.br/ws/${cep}/json/`;
+      const response = await this.axiosInstance.get(url);
 
-    const response = await axios.get(url);
+      if (response.data?.erro) {
+        throw new InvalidCepException('CEP inválido');
+      }
 
-    if (response.data.erro) {
-      throw new InvalidCepException('CEP inválido');
+      return response.data;
+    } catch (error: any) {
+      // Axios Error
+      if (error.code === 'ETIMEDOUT') {
+        throw new InvalidCepException(
+          'Tempo de resposta excedido ao consultar o CEP.'
+        );
+      }
+
+      if (error.code === 'ENETUNREACH') {
+        throw new InvalidCepException(
+          'Rede inacessível ao consultar o serviço de CEP.'
+        );
+      }
+
+      if (error.response?.status === 400) {
+        throw new InvalidCepException('CEP inválido.');
+      }
+
+      // Erro genérico
+      throw new InvalidCepException(
+        'Falha ao consultar o CEP. Tente novamente mais tarde.'
+      );
     }
-
-    return response.data;
   }
-
 }
